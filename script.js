a9033479e6cc371def6b78e500c7e597d7078f3c
Eunbi Go <eunbigo91@gmail.com>
2017-03-22 19:36:55 -0700

    Add: remove button

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
 
 
-(function() {
-  var app = angular.module('todo', []);
+var app = angular.module('todo', []);
 
-  app.controller('TodoCtrl', ['$scope', function($scope) {
-    $scope.todos = [
-        {
-            title: 'laundry',
-            completed: false,
-            createdAt: Date.now()
-        },
-        {
-            title: 'study',
-            completed: true,
-            createdAt: Date.now()
-        },
-        {
-            title: 'work out',
-            completed: false,
-            createdAt: Date.now()
-        }
+app.controller('TodoCtrl', ['$scope', function($scope) {
+$scope.todos = [
+    {
+        id: 1,
+        title: 'laundry',
+        completed: false,
+        createdAt: Date.now()
+    },
+    {
+        id: 2,
+        title: 'study',
+        completed: true,
+        createdAt: Date.now()
+    },
+    {
+        id: 3,
+        title: 'work out',
+        completed: false,
+        createdAt: Date.now()
+    }
     ];
-  }]);
-})();
+
+    $scope.remove = function(todo) {
+        // find todo index in todos
+        var idx = $scope.todos.findIndex(function(item) {
+            return item.id === todo.id;
+        });
+
+        // remove from todos
+        if (idx > -1) {
+            $scope.todos.splice(idx, 1);
+        };
+    }
+}]);
